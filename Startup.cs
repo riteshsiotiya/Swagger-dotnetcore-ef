2cbd67af5879fac76e53351706e0855cff7def42
riteshsiotiya <riteshsiotiya007@gmail.com>
2021-05-17 12:05:26 +0530

    First commit

---
+++ Startup.cs
@@ -0,0 +1,101 @@
+﻿using NSwag;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using AllCargoApiPoc.DB;
+using System.Reflection;
+using AllCargoApiPoc.Model;
+using AllCargoApiPoc.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NSwag.Generation.Processors.Security;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+
+namespace AllCargoApiPoc
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            var jwtTokenConfig = Configuration.GetSection("jwtTokenConfig").Get<JwtTokenConfig>();
+            services.AddSingleton(jwtTokenConfig);
+            services.AddSingleton<IJwtAuthManager, JwtAuthManager>();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = jwtTokenConfig.Issuer,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtTokenConfig.Secret)),
+                    ValidAudience = jwtTokenConfig.Audience,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.FromMinutes(1)
+                };
+                
+            });
+
+            services.AddOpenApiDocument(document =>
+            {
+                document.Title = "AllCargoApiPoc";
+                document.Version = "v1.0";
+                document.AddSecurity("JWT", Enumerable.Empty<string>(), new OpenApiSecurityScheme
+                {
+                    Type = OpenApiSecuritySchemeType.ApiKey,
+                    Name = "Authorization",
+                    In = OpenApiSecurityApiKeyLocation.Header,
+                    Scheme= "Bearer",
+                    Description = "Type into the textbox: Bearer {your JWT token}.",
+                });
+                document.OperationProcessors.Add(
+                    new AspNetCoreOperationSecurityScopeProcessor("JWT"));
+                
+            });
+            services.AddSwaggerGen(c =>
+            {
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+
+            services.AddMvc();
+            var ConnectionString = Configuration.GetConnectionString("AllCardoDBConnStrings");
+            services.AddDbContext<AllCargoDBContext>(options => options.UseSqlServer(ConnectionString));
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            var path = Directory.GetCurrentDirectory();
+            loggerFactory.AddFile($"{path}\\Logs\\Log.txt");
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseAuthentication();
+            app.UseMvc();
+            app.UseSwaggerUi3(c =>
+            {
+                c.DefaultModelsExpandDepth = -1;
+            });
+            app.UseOpenApi();
+        }
+    }
+}
